55d7085de5c3ee9726b5145bed77124b650380cf
DazzlingHT <Dazzling.HighTech@gmail.com>
2017-04-26 12:09:53 +0200

    Init project

---
+++ app/src/main/java/com/dhtech/funnytime/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dhtech.funnytime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
